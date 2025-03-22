6b65451932f89d67b4c543081103a1351e638b63
Lazar Gabriel <gabriellazar693@gmail.com>
2025-03-22 21:25:09 +0200

    finish

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,24 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/AlphaStore/", 
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Importuri scurte (ex: "@/components/Navbar")
+      "@": path.resolve(__dirname, "src"), 
     },
   },
   server: {
-    port: 3000, // Poți schimba portul, dacă ai nevoie
-    strictPort: true, // Dacă portul e ocupat, Vite nu va schimba automat portul
-    open: true, // Deschide automat browser-ul la pornire
+    port: 3000,
+    strictPort: true,
+    open: true,
   },
   build: {
-    outDir: "dist", // Directorul unde se generează build-ul
-    emptyOutDir: true, // Șterge conținutul vechi înainte de build
-    target: "esnext", // Optimizează pentru browsere moderne
+    outDir: "dist",
+    emptyOutDir: true,
+    target: "esnext",
   },
   define: {
-    "process.env": {}, // Evită erori legate de "process is not defined" în React
+    "process.env": {},
   },
 });
